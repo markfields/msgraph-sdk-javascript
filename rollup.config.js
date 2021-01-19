98d4bef4b7ba4a994d9f5ea42929dd2eb10495f8
Nikitha Chettiar <nikithauc@gmail.com>
2021-01-18 19:10:49 -0800

    Updating rollup plugins

--- rollup.config.js
+++ rollup.config.js
@@ -1,34 +1,35 @@
 import { terser } from "rollup-plugin-terser";
-import resolve from "rollup-plugin-node-resolve";
-import babel from "rollup-plugin-babel";
-import commonjs from "rollup-plugin-commonjs";
+import resolve from "@rollup/plugin-node-resolve";
+import babel from "@rollup/plugin-babel";
+import commonjs from "@rollup/plugin-commonjs";
 
-const config = [{
-        input: ["lib/es/src/browser/index.js"],
-        output: {
-            file: "lib/graph-es-sdk.js",
-            format: "es",
-            name: "MicrosoftGraph",
-        },
-        plugins: [resolve(), terser()],
-    },
-    {
-        input: ["lib/es/src/browser/index.js"],
-        output: {
-            file: "lib/graph-js-sdk.js",
-            format: "iife",
-            name: "MicrosoftGraph",
-        },
-        plugins: [
-            resolve(),
-            babel({
-                runtimeHelpers: true,
-                exclude: "node_modules/**",
-            }),
-            commonjs({ include: "node_modules/**" }),
-            terser(),
-        ],
-    },
+const config = [
+	{
+		input: ["lib/es/src/browser/index.js"],
+		output: {
+			file: "lib/graph-es-sdk.js",
+			format: "es",
+			name: "MicrosoftGraph",
+		},
+		plugins: [resolve(), terser()],
+	},
+	{
+		input: ["lib/es/src/browser/index.js"],
+		output: {
+			file: "lib/graph-js-sdk.js",
+			format: "iife",
+			name: "MicrosoftGraph",
+		},
+		plugins: [
+			resolve(),
+			babel({
+				babelHelpers: "runtime",
+				exclude: "node_modules/**",
+			}),
+			commonjs({ include: "node_modules/**" }),
+			terser(),
+		],
+	},
 ];
 
 export default config;
